a91a25a4ff1144e1f0982924c34ff6a336edead2
yaniaiciin2 <yani.aici@in2.es>
2023-12-20 17:51:39 +0100

    Fixed Tests

--- src/test/java/es/in2/blockchainconnector/service/impl/BrokerAdapterNotificationServiceImplTest.java
+++ src/test/java/es/in2/blockchainconnector/service/impl/BrokerAdapterNotificationServiceImplTest.java
@@ -182,7 +182,7 @@ class BrokerAdapterNotificationServiceImplTest {
 
 
         }
-}
+    }
 
     @Test
     void testProcessNotificationWithDeletionData_lastTransactionDeleted() throws JsonProcessingException, NoSuchAlgorithmException {
@@ -300,5 +300,53 @@ class BrokerAdapterNotificationServiceImplTest {
         }
 
 
+    }
+
+    @Test
+    void testProcessNotificationWithValidData_transactionEmpty() throws JsonProcessingException, NoSuchAlgorithmException {
+        Map<String, Object> validDataMap = new HashMap<>();
+        validDataMap.put("id", "id_value");
+        validDataMap.put("type", "type_value");
+        validDataMap.put("data", Collections.emptyMap());
+
+        BrokerNotification orionLDNotification = new BrokerNotification(
+                "id_value",
+                "type_value",
+                Collections.singletonList(validDataMap),
+                "subscriptionId_value",
+                "notifiedAt_value"
+
+        );
+        List<Transaction> mockTransaction = Collections.emptyList();
+
+        Transaction resultTransaction = Transaction.builder()
+                .id(UUID.randomUUID())
+                .transactionId("mockedProcessId")
+                .createdAt(Timestamp.valueOf("2021-01-01 00:00:00.000000000"))
+                .dataLocation("http://example.com/entity?hl=test")
+                .entityId("mockedEntityId")
+                .entityHash("")
+                .status(TransactionStatus.RETRIEVED)
+                .trader(TransactionTrader.CONSUMER)
+                .hash("")
+                .newTransaction(true)
+                .build();
+
+
+        try (MockedStatic<HttpClient> httpUtilsMockedStatic = Mockito.mockStatic(HttpClient.class)) {
+            when(transactionService.getTransaction(anyString())).thenReturn(Mono.just(mockTransaction));
+            when(objectMapper.writeValueAsString(any())).thenReturn("Mocked response");
+            when(transactionService.saveTransaction(any())).thenReturn(Mono.just(resultTransaction));
+            when(transactionService.saveTransaction(any(Transaction.class)))
+                    .thenReturn(Mono.just(resultTransaction));
+            httpUtilsMockedStatic.when(HttpClient::newHttpClient).thenReturn(httpClient);
+            when(httpClient.sendAsync(any(), any())).thenReturn(CompletableFuture.completedFuture(httpResponse));
+            when(httpResponse.statusCode()).thenReturn(200);
+            when(httpResponse.body()).thenReturn("Mocked response");
+            service.processNotification(orionLDNotification).subscribe();
+            verify(transactionService).getTransaction(anyString());
+
+
+        }
     }
 }
